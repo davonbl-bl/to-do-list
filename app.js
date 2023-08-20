e0bfa328a4d65f90d4240069b0d448d033df5857
davonbl-bl <davonbl@gmail.com>
2023-08-19 23:57:05 -0400

    added more functionality on the edit and delete button concerning localStorage

--- app.js
+++ app.js
@@ -30,17 +30,17 @@ const displayInfo = () => {
 
     
     if(!localStorage.getItem('content')){
-        const memoryObj = {
+        let memoryObj = [{
             id: 1,
             containedText: getText
-        }
+        }]
         wrapContent.setAttribute('id', 'container1')
         contentContainer.setAttribute('id', 'content1')
         containedText.setAttribute('id', 'text1')
         showDelBtn.setAttribute('id', 'del1')
         showEditBtn.setAttribute('id', 'edit1')
         
-        const jsonMemoryObj = JSON.stringify(memoryObj)
+        let jsonMemoryObj = JSON.stringify(memoryObj)
         localStorage.setItem('content', jsonMemoryObj)
     }
 
@@ -64,6 +64,15 @@ const displayInfo = () => {
         containedText.innerText = updatedText
         wrapContent.replaceChild(containedText, wrapContent.childNodes[0])
         wrapContent.replaceChild(showEditBtn,wrapContent.childNodes[1])
+
+        // memoryObj = [{
+        //     id: 1,
+        //     containedText: getText
+        // }]
+
+        let changeContent = JSON.parse(localStorage.getItem('content'))
+        changeContent[0].containedText = updatedText
+        localStorage.setItem('content', JSON.stringify(changeContent))
         // console.log(containedText.innerText)
         // console.log('testing')
     })
@@ -71,11 +80,40 @@ const displayInfo = () => {
     const parentContainer = document.querySelector('#parentContainer');
     parentContainer.append(contentContainer); 
 
+    showDelBtn.addEventListener('click', () => {
+        // console.log('testing delete button')
+        // console.log(JSON.parse(localStorage.getItem('content')))
+        let getAnswer = confirm('Are you sure you want to delete this specific to do task?')
+
+        if(getAnswer){
+            wrapContent.remove()
+            let removeElement =JSON.parse(localStorage.getItem('content'))
+            // console.log(removeElement)
+            removeElement.splice(0, 1)
+            // console.log('now: ', removeElement)
+            // console.log(removeElement.length)
+            let placeElementBack = JSON.stringify(removeElement)
+            localStorage.setItem('content', placeElementBack)
+            let getLength = JSON.parse(localStorage.getItem('content'))
+            // console.log(getLength.length)
+            if(getLength.length === 0){
+                localStorage.clear(); 
+            }
+            // let placeElementBack = JSON.stringify(removeElement)
+            // localStorage.setItem('content', placeElementBack)
+        }
+    })
+
 
 }
 
 clickBtn.addEventListener('click', displayInfo)
 
+// create the ability to add a due date for the task at hand (optional: can be a text box for teh due date)
+// adding at the bottom of the to do list add the time where it was submitted 
+// hidding the main input bar after clicking the edit button 
+// setting up an alert when the task is due 
+// providing the user the option on when the due date is near ---> example: alert 
 
 const deleteAllBtn = document.querySelector('#deleteAll')
 
